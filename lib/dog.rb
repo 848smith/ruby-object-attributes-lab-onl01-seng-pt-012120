3322461194d9741f47fd9ec1bec676ec2cc9fc31
Andrew Smith <848smith@gmail.com>
2020-02-11 06:13:47 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  def name=(dog_name)
+    @new_dog = dog_name
+  end
+  
+  def name
+    @new_dog
+  end
+  
+  def breed=(breed_name)
+    
+  end
+  
+  def breed
+    
+  end
+end
